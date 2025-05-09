a43a6201fafed44a8f214eec8c4d396a2411b33c
John Davis <misha19989@icloud.com>
2025-05-09 11:02:09 +0200

    Create BonusCalculator and BonusService

---
+++ src/main/java/de/szut/bonuscalculator/service/BonusCalculator.java
@@ -0,0 +1,11 @@
+package de.szut.bonuscalculator.service;
+
+import de.szut.bonuscalculator.model.Employee;
+
+public class BonusCalculator {
+    private BonusService bonusService;
+
+    public double calculateTotalBonus(Employee employee) {
+        return 0;
+    }
+}
---
+++ src/main/java/de/szut/bonuscalculator/service/BonusService.java
@@ -0,0 +1,10 @@
+package de.szut.bonuscalculator.service;
+
+public class BonusService {
+    private static final int maxBonus = 0;
+    private static final int minBonus = 0;
+
+    public double applyRestrictions(double bonus) {
+        return 0;
+    }
+}
