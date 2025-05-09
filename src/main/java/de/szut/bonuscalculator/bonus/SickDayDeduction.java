d1fd51ec4349145d00cb330149b7d89bcefa85fc
John Davis <misha19989@icloud.com>
2025-05-09 11:40:50 +0200

    Implement SickDayDeduction, run tests (green)

--- src/main/java/de/szut/bonuscalculator/bonus/SickDayDeduction.java
+++ src/main/java/de/szut/bonuscalculator/bonus/SickDayDeduction.java
@@ -3,12 +3,26 @@ package de.szut.bonuscalculator.bonus;
 import de.szut.bonuscalculator.model.Employee;
 
 public class SickDayDeduction extends BonusDecorator {
+    private static final double DEDUCTION_TIER_1 = 200.0; // 10-20 Fehltage
+    private static final double DEDUCTION_TIER_2 = 400.0; // über 20 Fehltage
+    private static final int TIER_1_MIN = 10;
+    private static final int TIER_2_MIN = 20;
+
     protected SickDayDeduction(Bonus decoratedBonus) {
         super(decoratedBonus);
     }
 
     @Override
     public double calculateBonus(Employee employee) {
-        return 0;
+        double baseBonus = decoratedBonus.calculateBonus(employee);
+        int sickDays = employee.getSickDays();
+
+        if (sickDays > TIER_2_MIN) {
+            return baseBonus - DEDUCTION_TIER_2;
+        } else if (sickDays > TIER_1_MIN) {
+            return baseBonus - DEDUCTION_TIER_1;
+        }
+
+        return baseBonus;
     }
 }
