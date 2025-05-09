4717bc0d222bc3997b1cb2cd0754c704e3466553
John Davis <misha19989@icloud.com>
2025-05-09 10:31:19 +0200

    Implement BasicBonus

--- src/main/java/de/szut/bonuscalculator/bonus/BasicBonus.java
+++ src/main/java/de/szut/bonuscalculator/bonus/BasicBonus.java
@@ -3,8 +3,10 @@ package de.szut.bonuscalculator.bonus;
 import de.szut.bonuscalculator.model.Employee;
 
 public class BasicBonus implements Bonus {
+    private static final double BASIC_BONUS_AMOUNT = 500.0;
+
     @Override
     public double calculateBonus(Employee employee) {
-        return 0;
+        return BASIC_BONUS_AMOUNT;
     }
 }
--- src/test/java/de/szut/bonuscalculator/bonus/BasicBonusTest.java
+++ src/test/java/de/szut/bonuscalculator/bonus/BasicBonusTest.java
@@ -19,7 +19,7 @@ class BasicBonusTest {
                 .isTeamLeader(false)
                 .build();
 
-        // When
+        // WhenEd
         double bonus = basicBonus.calculateBonus(employee);
 
         // Then
