f1509f5d7536351491ee863379c618c22933cfd6
John Davis <misha19989@icloud.com>
2025-05-09 10:55:52 +0200

    Implement SeniorityBonus, run tests

--- src/main/java/de/szut/bonuscalculator/bonus/SeniorityBonus.java
+++ src/main/java/de/szut/bonuscalculator/bonus/SeniorityBonus.java
@@ -3,12 +3,27 @@ package de.szut.bonuscalculator.bonus;
 import de.szut.bonuscalculator.model.Employee;
 
 public class SeniorityBonus extends BonusDecorator {
+    private static final int BASE_RATE_PER_YEAR = 10;
+    private static final int YEARS_THRESHOLD = 10;
+    private static final int ADDITIONAL_RATE = 10;
+
     protected SeniorityBonus(Bonus decoratedBonus) {
         super(decoratedBonus);
     }
 
     @Override
     public double calculateBonus(Employee employee) {
-        return 0;
+        double baseBonus = decoratedBonus.calculateBonus(employee);
+        return baseBonus + calculateSeniorityBonus(employee.getYearsOfService());
+    }
+
+    private double calculateSeniorityBonus(int years) {
+        if (years <= YEARS_THRESHOLD) {
+            return years * BASE_RATE_PER_YEAR;
+        } else {
+            int bonusRate = BASE_RATE_PER_YEAR + ADDITIONAL_RATE;
+            return (YEARS_THRESHOLD * BASE_RATE_PER_YEAR) +
+                    ((years - YEARS_THRESHOLD) * bonusRate);
+        }
     }
 }
