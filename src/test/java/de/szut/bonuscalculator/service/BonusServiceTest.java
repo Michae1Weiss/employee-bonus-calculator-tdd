7565e90c97bf7faac807242903fd4f673f0060a6
John Davis <misha19989@icloud.com>
2025-05-09 12:07:35 +0200

    Create and implement BonusServiceTest and three tests (fail)

---
+++ src/test/java/de/szut/bonuscalculator/service/BonusServiceTest.java
@@ -0,0 +1,46 @@
+package de.szut.bonuscalculator.service;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BonusServiceTest {
+
+    @Test
+    void shouldApplyMinimumRestriction() {
+        // Given
+        BonusService bonusService = new BonusService();
+        double calculatedBonus = 5.0; // Unter dem Minimum von 10
+
+        // When
+        double adjustedBonus = bonusService.applyRestrictions(calculatedBonus);
+
+        // Then
+        assertThat(adjustedBonus).isEqualTo(10.0);
+    }
+
+    @Test
+    void shouldApplyMaximumRestriction() {
+        // Given
+        BonusService bonusService = new BonusService();
+        double calculatedBonus = 1500.0; // Über dem Maximum von 1000
+
+        // When
+        double adjustedBonus = bonusService.applyRestrictions(calculatedBonus);
+
+        // Then
+        assertThat(adjustedBonus).isEqualTo(1000.0);
+    }
+
+    @Test
+    void shouldNotAdjustBonusWithinLimits() {
+        // Given
+        BonusService bonusService = new BonusService();
+        double calculatedBonus = 500.0; // Innerhalb der Grenzen
+
+        // When
+        double adjustedBonus = bonusService.applyRestrictions(calculatedBonus);
+
+        // Then
+        assertThat(adjustedBonus).isEqualTo(500.0);
+    }
+}
