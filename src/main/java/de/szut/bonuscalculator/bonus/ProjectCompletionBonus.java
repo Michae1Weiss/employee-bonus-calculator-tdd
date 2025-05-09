1f654979bcacc2d131910dc49bea446637802c9e
John Davis <misha19989@icloud.com>
2025-05-09 11:11:41 +0200

    Implement ProjectCompletionBonus class, run tests

--- src/main/java/de/szut/bonuscalculator/bonus/ProjectCompletionBonus.java
+++ src/main/java/de/szut/bonuscalculator/bonus/ProjectCompletionBonus.java
@@ -3,12 +3,17 @@ package de.szut.bonuscalculator.bonus;
 import de.szut.bonuscalculator.model.Employee;
 
 public class ProjectCompletionBonus extends BonusDecorator {
+    private static final double BONUS_PER_PROJECT = 10.0;
+
     protected ProjectCompletionBonus(Bonus decoratedBonus) {
         super(decoratedBonus);
     }
 
     @Override
     public double calculateBonus(Employee employee) {
-        return 0;
+        double baseBonus = decoratedBonus.calculateBonus(employee);
+        double projectCompletionBonus = employee.getCompletedProjects() * BONUS_PER_PROJECT;
+
+        return baseBonus + projectCompletionBonus;
     }
 }
