ccff6e50b03d562afce07f6cd3f0dd43abea7223
John Davis <misha19989@icloud.com>
2025-05-09 11:47:17 +0200

    Implement PerformanceBonus, run tests (green)

--- src/main/java/de/szut/bonuscalculator/bonus/PerformanceBonus.java
+++ src/main/java/de/szut/bonuscalculator/bonus/PerformanceBonus.java
@@ -3,12 +3,27 @@ package de.szut.bonuscalculator.bonus;
 import de.szut.bonuscalculator.model.Employee;
 
 public class PerformanceBonus extends BonusDecorator {
+    private static final double LOW_MULTIPLIER = 0.5;   // 0-3 Rating
+    private static final double MID_MULTIPLIER = 1.0;   // 4-7 Rating
+    private static final double HIGH_MULTIPLIER = 1.1;  // 8-10 Rating
+    private static final int LOW_MAX = 3;
+    private static final int MID_MAX = 7;
+
     protected PerformanceBonus(Bonus decoratedBonus) {
         super(decoratedBonus);
     }
 
     @Override
     public double calculateBonus(Employee employee) {
-        return 0;
+        double baseBonus = decoratedBonus.calculateBonus(employee);
+        int performanceRating = employee.getPerformanceRating();
+
+        if (performanceRating <= LOW_MAX) {
+            return baseBonus * LOW_MULTIPLIER;
+        } else if (performanceRating <= MID_MAX) {
+            return baseBonus * MID_MULTIPLIER;
+        } else {
+            return baseBonus * HIGH_MULTIPLIER;
+        }
     }
 }
