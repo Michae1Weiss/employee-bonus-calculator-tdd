7858a623f9f187d75fcff38997a5db4d3a896714
John Davis <misha19989@icloud.com>
2025-05-16 10:43:56 +0200

    Implement givenNullTeamleader_wennCalculateBonus_thenIllegalArgumentException test (fails)

--- src/test/java/de/szut/bonuscalculator/bonus/TeamLeaderBonusTest.java
+++ src/test/java/de/szut/bonuscalculator/bonus/TeamLeaderBonusTest.java
@@ -3,6 +3,7 @@ package de.szut.bonuscalculator.bonus;
 import de.szut.bonuscalculator.model.Employee;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 import static org.mockito.Mockito.*;
 
 public class TeamLeaderBonusTest {
@@ -33,5 +34,26 @@ public class TeamLeaderBonusTest {
         assertThat(bonusNonTeamLeader).isEqualTo(500.0); // unverändert
 
         verify(wrappedComponent, times(2)).calculateBonus(any(Employee.class));
+    }
+
+    @Test
+    void givenNullTeamleader_wennCalculateBonus_thenIllegalArgumentException() {
+        // Given
+        Bonus wrappedComponent = mock(Bonus.class);
+        when(wrappedComponent.calculateBonus(any(Employee.class))).thenReturn(500.0);
+
+        Bonus decorator = new TeamLeaderBonus(wrappedComponent);
+
+        // Fall 1: Teamleiter
+        Employee teamLeader = Employee.builder()
+                .build();
+
+        // When
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    double bonusNonTeamLeader = decorator.calculateBonus(teamLeader);
+                }).withMessageContaining("isTeamLeader boolean is null");
+
+        verify(wrappedComponent, times(1)).calculateBonus(any(Employee.class));
     }
 }
